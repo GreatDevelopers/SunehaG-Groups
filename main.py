1ee62b4f80a6351b92fd01b6e90b4c60d5b0b086
Amritpal Singh <amrit3701@gmail.com>
2016-11-05 22:41:52 +0530

    updates

--- main.py
+++ main.py
@@ -27,21 +27,17 @@ table3 = Table('student_data', metadata, autoload=True)
 
 #course_code
 var1 = table1.select(table1.c.course_code)
-m1 = [i for i in var1.execute()]
-print m1
 
 #branch_code
 var2 = table2.select(table2.c.course_code)
-m2 = [i for i in var2.execute()]
-print m2
 
 #student_data
 var3 = table3.select(table3.c.course_code)
-m3 = [i for i in var3.execute()]
-print m3
 
-#print [(student_data[7], course_code[1], branch_code[1], student_data[5], student_data[9], student_data[10]) for student_data in m3 for branch_code in m2 if str(student_data[1]) in str(branch_code[2]) and str(student_data[2]) in str(branch_code[0]) for course_code in m1 if str(student_data[1]) in course_code[0]]
+#list-comphrehension
+[(create_group(str(course_shortform[course_code[1]])), create_group(str(course_shortform[course_code[1]])+"_"+str(branch_shortform[branch_code[1]])), create_group(str(course_shortform[course_code[1]])+"_"+str(branch_shortform[branch_code[1]])+"_"+str(student_data[5])), create_group(str(course_shortform[course_code[1]])+"_"+str(branch_shortform[branch_code[1]])+"_"+str(student_data[5])+"_"+str(student_data[9])), create_group(str(course_shortform[course_code[1]])+"_"+str(branch_shortform[branch_code[1]])+"_"+str(student_data[5])+"_"+str(student_data[9])+"_"+      str(student_data[10]))) for student_data in var3.execute() for branch_code in var2.execute() if str(student_data[1]) in str(branch_code[2]) and str(student_data[2]) in str(branch_code[0]) for course_code in var1.execute() if str(student_data[1]) in course_code[0]]
 
+'''
 for student_data in m3:
    for branch_code in m2:
         if str(student_data[1]) in str(branch_code[2]) and str(student_data[2]) in str(branch_code[0]):
@@ -53,3 +49,4 @@ for student_data in m3:
                     section = str(course_shortform[course_code[1]])+"_"+str(branch_shortform[branch_code[1]])+"_"+str(student_data[5])+"_"+str(student_data[9])
                     group = str(course_shortform[course_code[1]])+"_"+str(branch_shortform[branch_code[1]])+"_"+str(student_data[5])+"_"+str(student_data[9])+"_"+str(student_data[10])
                     create_group(branch)
+'''
