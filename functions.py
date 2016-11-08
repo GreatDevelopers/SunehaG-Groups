e00dd6edca1553552ec4c489c4f6ed59e98c097f
Amritpal Singh <amrit3701@gmail.com>
2016-11-08 12:03:52 +0530

    updates

--- functions.py
+++ functions.py
@@ -3,35 +3,44 @@ import subprocess
 
 hostName="guru.gndec.ac.in"
 
-def addbookmark(username,group):
+def addbookmark(username,groups):
     xml = subprocess.Popen(["ejabberdctl","private_get",username,hostName,"storage","storage:bookmarks"], stdout=subprocess.PIPE).stdout.read()
     print xml
     ET.register_namespace("","storage:bookmarks")
     root = ET.fromstring(xml)
-    ET.SubElement(root,"conference",attrib={'jid':group,'autojoin':'true'})
+    for group in groups:
+        group = group.replace('/','_')
+        group = group.replace(' ','_')
+        ET.SubElement(root,"conference",attrib={'jid':group+"@conference."+hostName,'autojoin':'true'})
     output=ET.tostring(root)
     print(output)
     subprocess.Popen(["ejabberdctl","private_set",username,hostName,output])
 
 
-def send_invite(username,group):
-    group = group.replace('/','_')
-#    cmd="ejabberdctl send_direct_invitation "+group+" conference."+hostName+" none none "+username+"@"+hostName
-#    os.system(cmd)
-    #addbookmark(username,group+"@conference."+hostName)
+def send_invite(username,groups):
+    groups.append('GNDEC')
+    for group in groups:
+        group = group.replace('/','_')
+        group = group.replace(' ','_')
+        print group
+        cmd="ejabberdctl send_direct_invitation "+group+" conference."+hostName+" none none "+username+"@"+hostName
+        os.system(cmd)
+    addbookmark(username,groups)
 
 check_group = []
-def create_group(group):
-    group = group.replace('/','_')
-    if group not in check_group:
-        check_group.append(group)
-        #cmd="ejabberdctl create_room "+group+" conference."+hostName+" "+hostName
-        #os.system(cmd)
-        #cmd="ejabberdctl change_room_option "+group+" conference."+hostName+" allow_subscription true"
-        #os.system(cmd)
-        #cmd="ejabberdctl change_room_option "+group+" conference."+hostName+" persistent true"
-        #os.system(cmd)
-        #cmd="ejabberdctl change_room_option "+group+" conference."+hostName+" max_users 5000"
-        #os.system(cmd)
-        print "check group is working ", group
-    print check_group
+def create_group(groups):
+    groups.append('GNDEC')
+    for group in groups:
+        group = group.replace('/','_')
+        group = group.replace(' ','_')
+        if group not in check_group:
+            check_group.append(group)
+            cmd="ejabberdctl create_room "+group+" conference."+hostName+" "+hostName
+            os.system(cmd)
+            cmd="ejabberdctl change_room_option "+group+" conference."+hostName+" allow_subscription true"
+            os.system(cmd)
+            cmd="ejabberdctl change_room_option "+group+" conference."+hostName+" persistent true"
+            os.system(cmd)
+            cmd="ejabberdctl change_room_option "+group+" conference."+hostName+" max_users 5000"
+            os.system(cmd)
+            print "check group is working ", group
--- main.py
+++ main.py
@@ -11,18 +11,17 @@ branch_shortform = {'Civil Engineering':'CE', 'Computer Science and Engineering'
         'Electrical Engineering':'EE', 'Electronics and Communication Engineering':'ECE',\
         'Information Technology':'IT', 'Masters in Business Administration':'MBA',\
         'Masters in Computer Application':'MCA', 'Mechanical Engineering':'ME',\
-        'Production Engineering':'PE', 'Electrical Engineering':'EE',\
-        'Electronics and Communication Engineering':'ECE',\
+        'Production Engineering':'PE',\
         'Environmental Science Engineering':'ESE', 'Industrial Engineering':'IE',\
-        'Information Technology':'IT', 'Power Engineering':'PE', \
-        'Production Engineering':'PE', 'Structural Engineering':'SE',\
-        'VLSI Design':'VLSID', 'Computer Science and Engineering':'CSE',\
+        'Power Engineering':'PowE', \
+        'Structural Engineering':'SE',\
+        'VLSI Design':'VLSID',\
         'Energy Engineering':'EngE', 'Geo Technical Engineering ':'GEO',\
         'Soil Mechanics and Foundation Engineering':'SMFE'}
 
 metadata = MetaData(engine)
 table1 = Table('course_code', metadata, autoload=True)
-table2 = Table('branch_code', metadata, autoload=True)
+table2 = Table('branch_code', metadata, autoload=Trues
 table3 = Table('student_data', metadata, autoload=True)
 
 #course_code
@@ -35,18 +34,32 @@ var2 = table2.select(table2.c.course_code)
 var3 = table3.select(table3.c.course_code)
 
 #list-comphrehension
-[(create_group(str(course_shortform[course_code[1]])), create_group(str(course_shortform[course_code[1]])+"_"+str(branch_shortform[branch_code[1]])), create_group(str(course_shortform[course_code[1]])+"_"+str(branch_shortform[branch_code[1]])+"_"+str(student_data[5])), create_group(str(course_shortform[course_code[1]])+"_"+str(branch_shortform[branch_code[1]])+"_"+str(student_data[5])+"_"+str(student_data[9])), create_group(str(course_shortform[course_code[1]])+"_"+str(branch_shortform[branch_code[1]])+"_"+str(student_data[5])+"_"+str(student_data[9])+"_"+      str(student_data[10]))) for student_data in var3.execute() for branch_code in var2.execute() if str(student_data[1]) in str(branch_code[2]) and str(student_data[2]) in str(branch_code[0]) for course_code in var1.execute() if str(student_data[1]) in course_code[0]]
 
-'''
-for student_data in m3:
-   for branch_code in m2:
+#[(create_group([str(course_shortform[course_code[1]]),(str(course_shortform[course_code[1]])+"_"+str(branch_shortform[branch_code[1]])), (str(course_shortform[course_code[1]])+"_"+str(branch_shortform[branch_code[1]])+"_"+str(student_data[5])), (str(course_shortform[course_code[1]])+"_"+str(branch_shortform[branch_code[1]])+"_"+str(student_data[5])+"_"+str(student_data[9])), (str(course_shortform[course_code[1]])+"_"+str(branch_shortform[branch_code[1]])+"_"+str(student_data[5])+"_"+str(student_data[9])+"_"+str(student_data[10]))]), send_invite(str(student_data[7]),[(str(course_shortform[course_code[1]])+"_"+str(branch_shortform[branch_code[1]])), (str(course_shortform[course_code[1]])+"_"+str(branch_shortform[branch_code[1]])+"_"+str(student_data[5])), (str(course_shortform[course_code[1]])+"_"+str(branch_shortform[branch_code[1]])+"_"+str(student_data[5])+"_"+str(student_data[9])), (str(course_shortform[course_code[1]])+"_"+str(branch_shortform[branch_code[1]])+"_"+str(student_data[5])+"_"+str(student_data[9])+"_"+str(student_data[10]))])) for student_data in var3.execute() for branch_code in var2.execute() if str(student_data[1]) in str(branch_code[2]) and str(student_data[2]) in str(branch_code[0]) for course_code in var1.execute() if str(student_data[1]) in course_code[0]]
+
+
+#student_data[1]: course_code
+#branch_code[2]: course_code
+#student_data[2]: branch_code
+#branch_code[0]: branch_code
+#course_code[1]: course_name
+#course_code[0]: course_code
+#branch_code[1]: branch_name
+#student_data[5]: batch_year
+#student_data[9]: ssection
+#student_data[7]: college_roll_no
+#student_data[10]: sgroup
+
+for student_data in var3.execute():
+   for branch_code in var2.execute():
         if str(student_data[1]) in str(branch_code[2]) and str(student_data[2]) in str(branch_code[0]):
-            for course_code in m1:
+            for course_code in var1.execute():
                 if str(student_data[1]) in course_code[0]:
                     course = str(course_shortform[course_code[1]])
                     branch = str(course_shortform[course_code[1]])+"_"+str(branch_shortform[branch_code[1]])
                     passing_year = str(course_shortform[course_code[1]])+"_"+str(branch_shortform[branch_code[1]])+"_"+str(student_data[5])
                     section = str(course_shortform[course_code[1]])+"_"+str(branch_shortform[branch_code[1]])+"_"+str(student_data[5])+"_"+str(student_data[9])
                     group = str(course_shortform[course_code[1]])+"_"+str(branch_shortform[branch_code[1]])+"_"+str(student_data[5])+"_"+str(student_data[9])+"_"+str(student_data[10])
-                    create_group(branch)
-'''
+                    create_group([course, branch, passing_year, section, group])
+                    send_invite(str(student_data[7]),[course, branch, passing_year, section, group])
+                    print student_data[7]
