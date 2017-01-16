d6ee513e10085bac762c1433fe2cf0d7a938a724
amarjeet <amarjeet.kapoor1@gmail.com>
2017-01-16 20:36:59 +0530

    Converted into classes

--- functions.py
+++ functions.py
@@ -2,50 +2,71 @@ import os
 import xml.etree.ElementTree as ET
 import subprocess
 
-hostName="localhost"
+class sunehaG():
 
-def addbookmark(username,groups):
-    xml = subprocess.Popen(["ejabberdctl","private_get",username,hostName,"storage","storage:bookmarks"], stdout=subprocess.PIPE).stdout.read()
-    print xml
-    ET.register_namespace("","storage:bookmarks")
-    root = ET.fromstring(xml)
-    for group in groups:
-        group = group.replace('/','-')
-        group = group.replace(' ','-')
-        ET.SubElement(root,"conference",attrib={'jid':group+"@conference."+hostName,'autojoin':'true'})
-    output=ET.tostring(root)
-    print(output)
-    subprocess.Popen(["ejabberdctl","private_set",username,hostName,output])
+	def __init__(self,name):
+		self.hostName=name
+
+	def addbookmark(self,username,groups):
+		xml = subprocess.Popen(["ejabberdctl","private_get",username,
+		self.hostName,"storage","storage:bookmarks"], 
+		stdout=subprocess.PIPE).stdout.read()
+
+		print xml
+		ET.register_namespace("","storage:bookmarks")
+		root = ET.fromstring(xml)
+		for group in groups:
+			group = group.replace('/','-')
+			group = group.replace(' ','-')
+			ET.SubElement(root,"conference",attrib={'jid':group+"@conference."+
+			self.hostName,'autojoin':'true'})
+
+		output=ET.tostring(root)
+		print(output)
+		subprocess.Popen(["ejabberdctl","private_set",username,self.hostName,output])
 
 
-def send_invite(username,groups):
-    username = username.replace('/','_')
-    username = username.replace(' ','_')
-    for group in groups:
-        group = group.replace('/','-')
-        group = group.replace(' ','-')
-        print group
-        cmd="ejabberdctl set_room_affiliation "+group+" conference."+hostName+" "+username+"@"+hostName+" member"
-        os.system(cmd)
-        cmd="ejabberdctl send_direct_invitation "+group+" conference."+hostName+" none none "+username+"@"+hostName
-        os.system(cmd)
-    addbookmark(username,groups)
+	def send_invite(self,username,groups):
+		username = username.replace('/','_')
+		username = username.replace(' ','_')
+		for group in groups:
+			group = group.replace('/','-')
+			group = group.replace(' ','-')
+			print group
+			cmd="ejabberdctl set_room_affiliation "+group+" conference."+
+			self.hostName+" "+username+"@"+self.hostName+" member"
+			os.system(cmd)
+			cmd="ejabberdctl send_direct_invitation "+group+" conference."+
+			self.hostName+" none none "+username+"@"+self.hostName
+			os.system(cmd)
+		self.addbookmark(username,groups)
 
-check_group = []
-def create_group(groups):
-    for group in groups:
-        group = group.replace('/','-')
-        group = group.replace(' ','-')
-        if group not in check_group:
-            check_group.append(group)
-            cmd="ejabberdctl create_room "+group+" conference."+hostName+" "+hostName
-            os.system(cmd)
-            cmd="ejabberdctl change_room_option "+group+" conference."+hostName+" allow_subscription true"
-            os.system(cmd)
-            cmd="ejabberdctl change_room_option "+group+" conference."+hostName+" persistent true"
-            os.system(cmd)
-            cmd="ejabberdctl change_room_option "+group+" conference."+hostName+" max_users 5000"
-            os.system(cmd)
-            cmd="ejabberdctl change_room_option "+group+" conference."+hostName+" members_only true"
-            os.system(cmd)
-            print "check group is working ", group
+	check_group = []
+	def create_group(self,groups):
+		for group in groups:
+			group = group.replace('/','-')
+			group = group.replace(' ','-')
+			if group not in self.check_group:
+				self.check_group.append(group)
+				cmd="ejabberdctl create_room "+group+" conference."+
+				self.hostName+" "+self.hostName
+				os.system(cmd)
+
+				cmd="ejabberdctl change_room_option "+group+
+				" conference."+self.hostName+" allow_subscription true"
+
+				os.system(cmd)
+				cmd="ejabberdctl change_room_option "+group+
+				" conference."+self.hostName+" persistent true"
+				os.system(cmd)
+
+				cmd="ejabberdctl change_room_option "+group+
+				" conference."+self.hostName+" max_users 5000"
+				os.system(cmd)
+
+				cmd="ejabberdctl change_room_option "+group+
+				" conference."+self.hostName+" members_only true"
+				os.system(cmd)
+
+				print cmd
+				print "check group is working ", group, self.hostName
--- main.py
+++ main.py
@@ -7,22 +7,22 @@ from sqlalchemy import * # don't use * in production
 engine = create_engine('mysql://<username>:<password>@<host>/<database name>')
 
 #FIX: Replace with the hostName here
-hostName="HostName"
+sunehag=sunehaG("HostName")
 
 course_shortform = {'B.Tech.':'BT', 'M.Tech.':'MT', 'MBA':'MBA', 'MCA':'MCA'}
 
 #space FIXME
 branch_shortform = {'Civil Engineering':'CE', 'Computer Science and Engineering':'CSE', \
-        'Electrical Engineering':'EE', 'Electronics and Communication Engineering':'ECE',\
-        'Information Technology':'IT', 'Masters in Business Administration':'MBA',\
-        'Masters in Computer Application':'MCA', 'Mechanical Engineering':'ME',\
-        'Production Engineering':'PE',\
-        'Environmental Science Engineering':'ESE', 'Industrial Engineering':'IE',\
-        'Power Engineering':'PowE', \
-        'Structural Engineering':'SE',\
-        'VLSI Design':'VLSID',\
-        'Energy Engineering':'EngE', 'Geo Technical Engineering ':'GEO',\
-        'Soil Mechanics and Foundation Engineering':'SMFE'}
+	'Electrical Engineering':'EE', 'Electronics and Communication Engineering':'ECE',\
+	'Information Technology':'IT', 'Masters in Business Administration':'MBA',\
+	'Masters in Computer Application':'MCA', 'Mechanical Engineering':'ME',\
+	'Production Engineering':'PE',\
+	'Environmental Science Engineering':'ESE', 'Industrial Engineering':'IE',\
+	'Power Engineering':'PowE', \
+	'Structural Engineering':'SE',\
+	'VLSI Design':'VLSID',\
+	'Energy Engineering':'EngE', 'Geo Technical Engineering ':'GEO',\
+	'Soil Mechanics and Foundation Engineering':'SMFE'}
 
 #course_code
 connection = engine.connect()
@@ -46,18 +46,32 @@ student_query = text('select course_code, branch_code, ssection, sgroup, batch, 
 #student_data[5]: college_roll_no
 #student_data[3]: sgroup
 
-create_group(['GNDEC'])
+sunehag.create_group(['GNDEC'])
+
 for student_data in connection.execute(student_query):
-   for branch_code in connection.execute(branch_query):
-       if str(student_data[0]) in str(branch_code[2]) and str(student_data[1]) in str(branch_code[0]):
-           for course_code in connection.execute(course_query):
-               if str(student_data[0]) in str(course_code[0]):
-                    course = str(course_shortform[course_code[1]])
-                    branch = str(course_shortform[course_code[1]])+"-"+str(branch_shortform[branch_code[1]])
-                    passing_year = str(course_shortform[course_code[1]])+"_"+str(branch_shortform[branch_code[1]])+str(student_data[4])[-2:]
-                    section = str(course_shortform[course_code[1]])+"-"+str(branch_shortform[branch_code[1]])+str(student_data[4])[-2:]+"-"+str(student_data[2]).replace('/','')
-                    group = str(course_shortform[course_code[1]])+"-"+str(branch_shortform[branch_code[1]])+str(student_data[4])[-2:]+"-"+str(student_data[2]).replace('/','')+"-"+str(student_data[3])
-                    print course, branch, passing_year, section, group
-                    create_group([course, branch, passing_year, section, group])
-                    send_invite(str(student_data[5]),[course, branch, passing_year, section, group, 'GNDEC'])
-   print student_data[5]
+	for branch_code in connection.execute(branch_query):
+		if str(student_data[0]) in str(branch_code[2]) and str(student_data[1]) in str(branch_code[0]):
+			for course_code in connection.execute(course_query):
+				if str(student_data[0]) in str(course_code[0]):
+					course = str(course_shortform[course_code[1]])
+
+					branch = str(course_shortform[course_code[1]])+"-"+
+					str(branch_shortform[branch_code[1]])
+
+					passing_year = str(course_shortform[course_code[1]])+
+					"_"+str(branch_shortform[branch_code[1]])+str(student_data[4])[-2:]
+
+					section = str(course_shortform[course_code[1]])+"-"+
+					str(branch_shortform[branch_code[1]])+str(student_data[4])[-2:]+
+					"-"+str(student_data[2]).replace('/','')
+
+					group = str(course_shortform[course_code[1]])+"-"+
+					str(branch_shortform[branch_code[1]])+str(student_data[4])[-2:]+
+					"-"+str(student_data[2]).replace('/','')+"-"+str(student_data[3])
+
+					print course, branch, passing_year, section, group
+					sunehag.create_group([course, branch, passing_year, section, group])
+					sunehag.send_invite(str(student_data[5]),
+					[course, branch, passing_year, section, group, 'GNDEC'])
+
+	print student_data[5]
